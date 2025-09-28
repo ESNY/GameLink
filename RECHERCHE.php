15f4e3e27f970d8217eb317a78fdafe971a1ec1c
ESNY <khadraouilyes@gmail.com>
2025-09-29 01:35:23 +0200

    RECHERCHE màj + JS + CSS

--- RECHERCHE.php
+++ RECHERCHE.php
@@ -5,6 +5,8 @@
         <meta name="description"content="ACCUEIL GameLink">
         <title>Recherche | GameLink</title>
         <link rel="stylesheet" href="CSS/HEADER.css" type="text/css"/>
+        <link rel="stylesheet" href="CSS/STYLE_RECHERCHE.css" type="text/css"/>
+        <link rel="stylesheet" href="/PA/CSS/GAME_CARD.css">
         <link rel="icon" type="image/png" sizes="32x32" href="ICON/LogoFinal.svg">
     </head>
     <body>
@@ -24,7 +26,29 @@
         </header>
         
         <main>
-         
+            <section class="search-area">
+                <form class="search" role="search" action="RECHERCHE.php" method="get"> 
+                    <input type="text" type="search"
+                        placeholder="Titre du jeu, platforme, genre, etc..." 
+                        autocomplete="off"/>
+                    <img src="ICON/loupe.png" alt="Icône de loupe" />
+                    
+                </form>
+
+                <div class="filters">
+                    <button class="chip">Plateformes</button>
+                    <button class="chip">Genres</button>
+                    <button class="chip">Autres</button>
+                    <button class="chip">Note</button>
+                    <button class="chip">Trier</button>
+                    <button class="chip">Date</button>
+                </div>
+            </section>
+
+            <section>
+                <ul id="Game-list"></ul>
+            </section>  
+        
         </main>
-         
+        <script src="/PA/JS/RECHERCHE.js?v=3" defer></script>
     </body>
